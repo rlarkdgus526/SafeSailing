30d872d0b7cedb7a0b37994656c18eb6ece4d77e
kimkanghyeon <rlarkdgus526@naver.com>
2021-12-10 12:46:24 +0900

    Initial Commit

---
+++ SafeSailing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SafeSailing
+//
+//  Created by 김강현 on 2021/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SafeSailing/SafeSailingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSailingApp.swift
+//  SafeSailing
+//
+//  Created by 김강현 on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SafeSailingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
